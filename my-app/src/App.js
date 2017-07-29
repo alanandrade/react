c3a7cb784aa8a94b40eb9b065dd57a59174f594c
Alan Andrade <alan.andradebispo@gmail.com>
2017-07-29 09:20:04 -0300

    Primeiro component React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,19 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HelloWorld from './HelloWorld'; //Nosso primeiro component React
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hello World - Bem Vindo</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <HelloWorld/>;
   }
 }
 
