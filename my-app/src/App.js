0527dea6d04b68af4c828783ef19893aa70a2b0f
Alan Andrade <alan.andradebispo@gmail.com>
2017-07-29 20:26:13 -0300

    dois ou mais componentes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import HelloWorld from './HelloWorld'; //Nosso primeiro component React
+import Header from './Header';
 
 class App extends Component {
   render() {
-    return <HelloWorld/>;
+    return <div>
+    <HelloWorld/>
+    <Header/>
+    </div>
   }
 }
 
