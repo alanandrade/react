5de252f658cf3c727fe5f627f49047c720e4b1e8
Alan Andrade <alan.andradebispo@gmail.com>
2017-08-09 22:48:52 -0300

    insercao de variaveis, links e link routers

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import logo from '../imagens/logo.svg'
 
+var frutas = ['Banana', 'Maçã', 'Uva'];
+
+var teste = "Teste Variavel";
+
 export default class Header extends React.Component {
+
 	render() {
 		return(
-		<div> 
+		<div>
+			<h3>{teste}</h3>
+
+		<div>
+			<ul>
+			{
+				frutas.map(function (fruta){
+					return <li>{fruta}</li>
+				})
+			}
+			</ul>
+		</div>
 		<h1>Teste!!!!</h1>
 		<img src={logo} className="App-logo" alt="logo" />
+		<a href="https://www.google.com.br/" target="__blank">link pagina</a>
 		</div>
 		);
 	}
 } 
+
+//https://css-tricks.com/learning-react-router/ | Rotas
